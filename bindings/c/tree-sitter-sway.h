55c5690ebe22094b5aca6894c85e4397544bc55b
Riley Bruins <ribru17@hotmail.com>
2024-11-11 08:36:55 -0800

    feat: bindings generated from 0.24.4

---
+++ bindings/c/tree-sitter-sway.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_SWAY_H_
+#define TREE_SITTER_SWAY_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_sway(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_SWAY_H_
